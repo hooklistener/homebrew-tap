bded0d1e8c6d5bf7955de1c410a84b65e108af2c
Adrian Carayol Orenes <adriancarayol@gmail.com>
2026-07-06 22:55:19 +0200

    Update hooklistener to 1.2.1

--- Formula/hooklistener.rb
+++ Formula/hooklistener.rb
@@ -4,25 +4,25 @@
 class Hooklistener < Formula
   desc "CLI tool for webhook inspection and local tunnel exposure"
   homepage "https://github.com/hooklistener/hooklistener-cli"
-  version "1.2.0"
+  version "1.2.1"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/hooklistener/hooklistener-cli/releases/download/v#{version}/hooklistener-aarch64-apple-darwin.tar.gz"
-      sha256 "0a7800dd645456c1086c05c7717ba3e028b83c3dfab6a497f253696e14476e6e"
+      sha256 "28cbb47b3fa16964b2c9d79080e6487234fc79f1609f80171122f6171fb7c492"
     end
 
     on_intel do
       url "https://github.com/hooklistener/hooklistener-cli/releases/download/v#{version}/hooklistener-x86_64-apple-darwin.tar.gz"
-      sha256 "121b57d48b17c6f436dc61b0ae823a618d0567d5d62ae65907625049159b1a08"
+      sha256 "4ee2e34a199e66e70763e9c6c490999b0c6225eab71549ff47d9151f3d68adb2"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/hooklistener/hooklistener-cli/releases/download/v#{version}/hooklistener-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "25652cb31d14245696aa726c9f111f3f9dd28e39549dfa8949d44dcb44b18d03"
+      sha256 "7e64c7bdcd87064e1ed7334ab9c54a24a1e465ab1cb855bddc659170588a1acd"
     end
   end
 
